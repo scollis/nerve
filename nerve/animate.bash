590c0a067c38e08800a44005f9562fb3960037e7
Scott Collis <scollis.acrf@gmail.com>
2018-06-08 12:58:08 -0500

    FIX: removed time gap for proccessing

--- nerve/animate.bash
+++ nerve/animate.bash
@@ -3,6 +3,8 @@ cdir=$(pwd)
 cd $1
 foo=$(ls KLOT_20180608_[0,1,2][0-9][0-9][0-9]_reflectivity.jpg | tail -n15)
 foo_zdr=$(ls KLOT_20180608_[0,1,2][0-9][0-9][0-9]_differential_reflectivity.jpg | tail -n15)
-convert -delay 30 $foo klot_reflectivity_animation.gif
-convert -delay 30 $foo_zdr klot_differential_reflectivity_animation.gif
+convert -delay 30 $foo klot_reflectivity_animation_temp.gif
+cp klot_reflectivity_animation_temp.gif klot_reflectivity_animation.gif
+convert -delay 30 $foo_zdr klot_differential_reflectivity_animation_temp.gif
+cp klot_differential_reflectivity_animation_temp.gif klot_differential_reflectivity_animation.gif
 cd $cdir
