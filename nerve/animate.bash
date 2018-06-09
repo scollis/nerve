bf842428ca441e79cc8039efc47b32b6ca47e657
Scott Collis <scollis.acrf@gmail.com>
2018-06-09 10:16:51 -0500

    FIX: bug in animate

--- nerve/animate.bash
+++ nerve/animate.bash
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 cdir=$(pwd)
 cd $1
-foo=$(ls KLOT_20180608_[0,1,2][0-9][0-9][0-9]_reflectivity.jpg | tail -n15)
-foo_zdr=$(ls KLOT_20180608_[0,1,2][0-9][0-9][0-9]_differential_reflectivity.jpg | tail -n15)
+foo=$(ls KLOT_*_[0,1,2][0-9][0-9][0-9]_reflectivity.jpg | tail -n15)
+foo_zdr=$(ls KLOT_*_[0,1,2][0-9][0-9][0-9]_differential_reflectivity.jpg | tail -n15)
 convert -delay 30 $foo klot_reflectivity_animation_temp.gif
 cp klot_reflectivity_animation_temp.gif klot_reflectivity_animation.gif
 convert -delay 30 $foo_zdr klot_differential_reflectivity_animation_temp.gif
